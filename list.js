1ae5276142439aae492e7299a4c7b717641a4e68
kailyn-carreira <kay1163@hotmail.com>
2022-05-03 15:34:04 -0400

    start

--- list.js
+++ list.js
@@ -1,2 +1,22 @@
 let taskList = [];
-const listDisplay = document.querySelector();
+const listDisplay = document.querySelector('[displayList]');
+const addTaskForm = document.querySelector('.addTask');
+const newTask = document.querySelector('[newTask]')
+
+function start() {
+    displayList();
+}
+function displayList() {
+    taskList.forEach(task => {
+        listDisplay.appendChild(document.createElement('li', ))
+    })
+}
+
+addTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = newTask.nodeValue;
+    taskList.push({
+        task: task,
+        isComplete: false,
+    });
+})
