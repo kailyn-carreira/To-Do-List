80717069c9b0d9d5d971884a80f593d0671ec4ee
kailyn-carreira <kay1163@hotmail.com>
2022-05-03 18:34:22 -0400

    rough draft

--- list.js
+++ list.js
@@ -1,22 +1,42 @@
-let taskList = [];
-const listDisplay = document.querySelector('[displayList]');
-const addTaskForm = document.querySelector('.addTask');
-const newTask = document.querySelector('[newTask]')
+const newTaskinput = document.querySelector('[data-new-task-input]')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const toDoList = document.querySelector('[data-task-list]')
 
-function start() {
-    displayList();
-}
-function displayList() {
-    taskList.forEach(task => {
-        listDisplay.appendChild(document.createElement('li', ))
+const LOCAL_STORAGE_LIST_KEY = 'toDo.List';
+
+let currentTasks = [{id: 0, tasks: 'hello', isComplete: false}];
+
+//This function creates the display of all the tasks and the buttons
+function display() {
+    while(toDoList.firstChild){
+        toDoList.removeChild(toDoList.firstChild)
+    }
+    currentTasks.forEach( (task) => {
+        const taskLi = document.createElement('li')
+        const taskStrong = document.createElement('strong')
+        const taskEdit = document.createElement('button')
+        const taskDelete = document.createElement('button')
+        taskStrong.innerText = `${task.task}`
+        taskEdit.innerText = 'Edit'
+        taskDelete.innerText = 'Delete'
+        taskLi.appendChild(taskStrong)
+        taskLi.appendChild(taskEdit)
+        taskLi.appendChild(taskDelete)
+        console.log(task);
+
+        toDoList.appendChild(task1) 
     })
 }
 
-addTaskForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const task = newTask.nodeValue;
-    taskList.push({
-        task: task,
-        isComplete: false,
-    });
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(currentTasks)
+    const newTask = newTaskinput.value
+    currentTasks.push({ id: currentTasks.length, task: newTask, isComplete: false})
+    localStorafe.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(currentTasks))
+    display();
 })
+
+
+
+
